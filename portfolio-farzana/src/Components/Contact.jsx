f55bd694a4c4fbf6cbcfea274687a2879de623c8
Farzana Mohsin <fmohsin@usertesting.com>
2024-07-04 09:31:03 -0400

    change styling to contatct section

--- portfolio-farzana/src/Components/Contact.jsx
+++ portfolio-farzana/src/Components/Contact.jsx
@@ -35,10 +35,7 @@ const Contact = () => {
       <section className='py-6 bg-gray-800 text-gray-50'>
         <div className='grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x'>
           <div className='py-6 md:py-0 md:px-6'>
-            <h1 className='text-4xl font-bold'>Get in touch</h1>
-            <p className='pt-2 pb-4'>
-              Fill in the form to start a conversation
-            </p>
+            <h1 className='text-4xl font-bold my-4'>Get in touch</h1>
             <div className='space-y-4'>
               <p className='flex items-center'>
                 <svg
@@ -53,7 +50,7 @@ const Contact = () => {
                     clipRule='evenodd'
                   ></path>
                 </svg>
-                <span>Fake address, 9999 City</span>
+                <span>Toronto, Ontario, Canada</span>
               </p>
               <p className='flex items-center'>
                 <svg
@@ -64,7 +61,7 @@ const Contact = () => {
                 >
                   <path d='M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z'></path>
                 </svg>
-                <span>123456789</span>
+                <span>+123456789</span>
               </p>
               <p className='flex items-center'>
                 <svg
@@ -76,50 +73,57 @@ const Contact = () => {
                   <path d='M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z'></path>
                   <path d='M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z'></path>
                 </svg>
-                <span>contact@business.com</span>
+                <span>farzanamohsinmohona@gmail.com</span>
               </p>
             </div>
           </div>
-          <form
-            ref={form}
-            onSubmit={sendEmail}
-            noValidate=''
-            className='flex flex-col py-6 space-y-6 md:py-0 md:px-6'
-          >
-            <label className='block'>
-              <span className='mb-1'>Full name</span>
-              <input
-                type='text'
-                name='user_name'
-                placeholder='Leroy Jenkins'
-                className='block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 bg-gray-800'
-              />
-            </label>
-            <label className='block'>
-              <span className='mb-1'>Email address</span>
-              <input
-                type='email'
-                name='user_email'
-                placeholder='leroy@jenkins.com'
-                className='block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 bg-gray-800'
-              />
-            </label>
-            <label className='block'>
-              <span className='mb-1'>Message</span>
-              <textarea
-                name='message'
-                rows='3'
-                className='block w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 bg-gray-800'
-              ></textarea>
-            </label>
-            <button
-              type='submit'
-              value='Send'
-              className='self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 bg-violet-400 text-gray-900 focus:ring-violet-400 hover:ring-violet-400'
+
+          <div>
+            <p className='pt-2 pb-4 text-center text-xl'>
+              Fill in the form to start a conversation
+            </p>
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+              noValidate=''
+              className='flex flex-col py-6 space-y-6 md:py-0 md:px-6'
             >
-              Submit
-            </button>
-          </form>
+              <label className='block'>
+                <span className='mb-1'>Full name</span>
+                <input
+                  type='text'
+                  name='user_name'
+                  placeholder='Your Name'
+                  className='block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 text-gray-500 p-1'
+                />
+              </label>
+              <label className='block'>
+                <span className='mb-1'>Email address</span>
+                <input
+                  type='email'
+                  name='user_email'
+                  placeholder='Your Email'
+                  className='block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-violet-400 text-gray-500 p-1'
+                />
+              </label>
+              <label className='block'>
+                <span className='mb-1'>Message</span>
+                <textarea
+                  placeholder='Your Message'
+                  name='message'
+                  rows='3'
+                  className='p-1 block w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-violet-400 text-gray-500'
+                ></textarea>
+              </label>
+              <button
+                type='submit'
+                value='Send'
+                className='self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 bg-violet-400 text-gray-900 focus:ring-violet-400 hover:ring-violet-400'
+              >
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </section>
     </div>
